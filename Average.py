7f437dc5d4313dd77196be48ef397f9506022241
RainerAvil2025 <raineravil3@gmail.com>
2025-04-17 16:15:28 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,10 @@
+#storing values
+tree1=19
+tree2=94
+tree3=41
+tree4=19
+tree5=11
+sum=tree1+tree2+tree3+tree4+tree5
+avg=sum/5
+print("The sum of all 5 trees is",sum)
+print("The average of all trees",avg)
